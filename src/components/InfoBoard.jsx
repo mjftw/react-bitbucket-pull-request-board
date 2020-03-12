0c606c3c5511783352f1b8c7be84f8a3b1e4afa5
Merlin Webster <mjftwebster@gmail.com>
2020-03-12 12:09:14 +0000

    InfoBoard displays message if no open PRs to display

--- src/components/InfoBoard.jsx
+++ src/components/InfoBoard.jsx
@@ -3,18 +3,23 @@ import Container from '@material-ui/core/Container'
 import PullRequestInfo from './PullRequestInfo';
 
 export default function InfoBoard(props) {
-    if (props.prData) {
+    if (props.prData === null || props.prData === undefined) {
+        return (
+            <h1>Repo data missing</h1>
+        );
+    }
+    else if (props.prData.length === 0) {
+        return (
+            <h1>No open pull requests to display...</h1>
+        );
+    }
+    else {
         return (
             <Container style={{ margin: '0.5rem' }}>
                 {props.prData.map(prDataItem =>
                     <PullRequestInfo prData={prDataItem}></PullRequestInfo>
                 )}
             </Container >
-        );
-    }
-    else {
-        return (
-            <h1>Repo data missing</h1>
         );
     }
 }

f0da5443690a7940543eede7478f39739ddde77c
Merlin Webster <mjftwebster@gmail.com>
2020-06-16 22:30:39 +0100

    Removed unused actions

--- src/redux/pullRequests/actions.js
+++ src/redux/pullRequests/actions.js
@@ -2,13 +2,9 @@ import {inArray} from '../../utils/array';
 import {getRepoPRDataPromises} from '../../utils/bitbucket';
 import {handleRequestError} from '../commonActions';
 import {
-    ADD_PULL_REQUESTS_FOR_REPO,
     REMOVE_PULL_REQUESTS_FOR_REPO,
     ADD_PULL_REQUEST,
-    UPDATE_PULL_REQUEST,
     FETCH_PULL_REQUESTS_FOR_REPO_BEGIN,
-    FETCH_PULL_REQUEST_BEGIN,
-    FETCH_PULL_REQUEST_SUCCESS,
     FETCH_PULL_REQUEST_FAILURE,
     FETCH_PULL_REQUEST_CANCELLED,
     FETCH_PULL_REQUESTS_FOR_REPO_END
